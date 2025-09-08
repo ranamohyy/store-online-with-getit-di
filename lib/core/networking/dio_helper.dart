c0c25dd35beac4652a48abf75868a965b62b0db8
ranamohyy <ranaamr22@gmail.com>
2025-09-08 22:40:17 +0300

    finish dio  helper using di

--- lib/core/networking/dio_helper.dart
+++ lib/core/networking/dio_helper.dart
@@ -1,20 +1,20 @@
 import 'dart:developer';
-
 import 'package:dio/dio.dart';
 import 'package:implement_clean_code_for_store/core/networking/end_ponits.dart';
 import 'package:pretty_dio_logger/pretty_dio_logger.dart';
 
 class DioHelper {
-  static Dio? dio;
-
-  static initDio() {
+   Dio? dio;
+DioHelper(){initDio();}
+   initDio() {
     dio ??= Dio(BaseOptions(
+      validateStatus: (status) => true,
         baseUrl: EndPoints.baseUrl,
         receiveDataWhenStatusError: true));
     dio!.interceptors.add(PrettyDioLogger());
   }
 
-  static getRequest(
+   getRequest(
       {required String path, required Map<String, dynamic> data}) async {
     try {
       Response response = await dio!.get(path, queryParameters: data);
@@ -25,14 +25,14 @@ class DioHelper {
       );
     }
   }
+    postRequest(
+       {required String path, required Map<String, dynamic> data}) async {
+     try {
+       Response response = await dio!.post(path, data: data);
+       return response;
+     } catch (e) {
+       rethrow;
+     }
 
-  static postRequest(
-      {required String path, required Map<String, dynamic> data}) async {
-    try {
-      Response response = await dio!.post(path, data: data);
-      return response;
-    } catch (e) {
-      rethrow;
-    }
   }
 }
