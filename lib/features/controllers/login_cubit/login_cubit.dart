f72e11c7d0ba74d53e449899d755396b40f7628b
ranamohyy <ranaamr22@gmail.com>
2025-09-08 22:41:10 +0300

    finish login cubit

---
+++ lib/features/controllers/login_cubit/login_cubit.dart
@@ -0,0 +1,34 @@
+import 'package:animated_snack_bar/animated_snack_bar.dart';
+import 'package:dartz/dartz.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:implement_clean_code_for_store/core/repo/auth_repo.dart';
+import 'package:implement_clean_code_for_store/features/models/user_model.dart';
+part 'login_state.dart';
+
+class LoginCubit extends Cubit<LoginState> {
+  AuthRepo authRepo;
+  LoginCubit(this.authRepo) : super(LoginInitial());
+  final TextEditingController nameController=TextEditingController();
+  final TextEditingController passwordController=TextEditingController();
+
+   void login()async{
+     emit(LoginLoading());
+   final Either<String,UserModel> response=
+   await authRepo.login(userName: nameController.text, password: passwordController.text);
+   response.fold(
+       (error) {
+         emit(LoginFailure(error));}
+     ,(r) {
+     emit(LoginSuccess());
+       },
+   );
+
+  }
+
+
+
+
+
+
+}
