49cf4f3927b51620d92579d6e4c305948e722b10
ranamohyy <ranaamr22@gmail.com>
2025-09-08 22:38:24 +0300

    finish login repo using di

--- lib/core/repo/auth_repo.dart
+++ lib/core/repo/auth_repo.dart
@@ -1,27 +1,38 @@
 import 'package:dartz/dartz.dart';
 import 'package:dio/dio.dart';
+import 'package:implement_clean_code_for_store/core/helpers/cache_helper.dart';
 import 'package:implement_clean_code_for_store/core/networking/dio_helper.dart';
 import 'package:implement_clean_code_for_store/core/networking/end_ponits.dart';
 import 'package:implement_clean_code_for_store/features/models/user_model.dart';
 
+import '../di/get_it.dart';
+
 class AuthRepo {
+  final DioHelper _dioHelper;
+  AuthRepo(this._dioHelper);
   Future<Either<String, UserModel>> login(
       {required String userName, required String password}) async {
     final Response response =
-        await DioHelper.postRequest(path: EndPoints.login, data: {
+        await _dioHelper.postRequest(path: EndPoints.login, data: {
       "username": userName,
       "password": password,
     });
     try {
-      if (response.statusCode == 200) {
-        UserModel userModel = UserModel();
-        return Right(userModel);
+      if (response.statusCode == 200||response.statusCode==201) {
+        UserModel userModel = UserModel.fromJson(response.data);
+        if(userModel.token !=null){
+          await getIt<CacheHelper>().saveToken(userModel.token!);
+          return Right(userModel);
+        }else{
+          return Left("Token is Null");
+        }
       } else {
-        return Left(response.toString());
+
+        return Left(response.data.toString());
       }
     } catch (e) {
       if(e is DioException){
-        return Left(e.response.toString());
+        return Left(e.response?.data.toString()??e.message??"error");
       }
       return Left(e.toString());
     }
