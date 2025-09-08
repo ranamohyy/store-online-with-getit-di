9c708965698f716815b33e30f4a60189840a8399
ranamohyy <ranaamr22@gmail.com>
2025-09-08 22:32:02 +0300

    modeling user

--- lib/features/models/user_model.dart
+++ lib/features/models/user_model.dart
@@ -4,9 +4,4 @@ class UserModel{
   UserModel.fromJson(Map<String,dynamic>json){
     token=json['token'];
   }
-   Map<String,dynamic> toJson(){
-    final Map<String,dynamic>data=<String,dynamic>{};
-    data['token']=token;
-    return data;
-}
 }
