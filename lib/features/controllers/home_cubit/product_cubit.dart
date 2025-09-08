fa34ca0f235945cf20a4e8518435f7a3c45b8359
ranamohyy <ranaamr22@gmail.com>
2025-09-08 22:37:09 +0300

    make Get home's product cubit

---
+++ lib/features/controllers/home_cubit/product_cubit.dart
@@ -0,0 +1,23 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:implement_clean_code_for_store/core/repo/home_repo.dart';
+import 'package:implement_clean_code_for_store/features/models/product_model.dart';
+part 'product_state.dart';
+
+class ProductCubit extends Cubit<ProductState> {
+  ProductCubit(this.repo) : super(ProductInitial());
+  HomeRepo repo;
+
+  getAllProducts() async {
+    emit(ProductLoading());
+    final Either<String, List<ProductModel>> response
+    = await repo.getAllProducts();
+
+    response.fold(
+      (error) {
+        emit(ProductFailure(error));
+      },
+      (model) => emit(ProductSuccess(model)),
+    );
+  }
+}
