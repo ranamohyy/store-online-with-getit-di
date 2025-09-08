e78c1f5dc0a0763ee80a4431c099078b0e811f71
ranamohyy <ranaamr22@gmail.com>
2025-09-08 22:38:43 +0300

    add styles

---
+++ lib/core/helpers/app_text_styles.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:implement_clean_code_for_store/core/helpers/app_colors.dart';
+
+class AppStyles{
+
+  static const textStyle16Black=TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w500,
+    color: Colors.black
+
+  );
+  static const textStyle16Grey=TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w500,
+    color: AppColors.greyColor
+
+  );
+  static const textStyle14white=TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w500,
+    color: Colors.white
+
+  );
+static const textStyle12Grey=TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.w600,
+    color: AppColors.greyColor
+
+  );
+  static const textStyle32Black=TextStyle(
+      fontSize: 32,
+      fontWeight: FontWeight.w700,
+      color: Colors.black
+
+  );
+
+
+}
