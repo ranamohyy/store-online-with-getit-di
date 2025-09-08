1a6d88aaf74b3f029823aecf6b60bc04c229c1eb
ranamohyy <ranaamr22@gmail.com>
2025-09-08 22:38:54 +0300

    add colors

---
+++ lib/core/helpers/app_colors.dart
@@ -0,0 +1,15 @@
+
+import 'package:flutter/material.dart';
+
+class AppColors{
+
+  static const greyColor=Color(0xff808080);
+  static const boldGreyColor=Color(0xff999999);
+  static const scaffoldBackGroundColor=Colors.white;
+  static const fieldBorderColor=Color(0xffE6E6E6);
+  static const primaryColor=Color(0xff3669C9);
+
+
+
+}
+
