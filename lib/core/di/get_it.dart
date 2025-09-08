cb9b4b0b3bb51b84e0c28d8486c667eec6cf4009
ranamohyy <ranaamr22@gmail.com>
2025-09-08 22:42:34 +0300

    use get it to make di

---
+++ lib/core/di/get_it.dart
@@ -0,0 +1,23 @@
+
+import"package:get_it/get_it.dart";
+import "package:implement_clean_code_for_store/core/helpers/cache_helper.dart";
+import "package:implement_clean_code_for_store/core/networking/dio_helper.dart";
+import "package:implement_clean_code_for_store/core/repo/auth_repo.dart";
+import "package:implement_clean_code_for_store/core/repo/home_repo.dart";
+import "package:implement_clean_code_for_store/features/controllers/home_cubit/product_cubit.dart";
+import "package:implement_clean_code_for_store/features/controllers/login_cubit/login_cubit.dart";
+  final getIt = GetIt.instance;
+ void setup() {
+   getIt.registerLazySingleton(()=>CacheHelper());
+
+   DioHelper dio=DioHelper();
+    getIt.registerSingleton<DioHelper>((dio));
+    //repos
+    getIt.registerLazySingleton<AuthRepo>(() => AuthRepo(getIt()),);
+    getIt.registerLazySingleton<HomeRepo>(() => HomeRepo(getIt()),);
+
+    //cubit
+    getIt.registerLazySingleton<LoginCubit>(() => LoginCubit(getIt()),);
+    getIt.registerFactory<ProductCubit>(() => ProductCubit(getIt()));
+
+}
