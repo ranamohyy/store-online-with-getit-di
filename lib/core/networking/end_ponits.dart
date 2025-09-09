e62f219340fe1e9f62818dafbd5b80efc7ec463e
ranamohyy <ranaamr22@gmail.com>
2025-09-09 03:30:39 +0300

    add end points

--- lib/core/networking/end_ponits.dart
+++ lib/core/networking/end_ponits.dart
@@ -2,6 +2,7 @@ class EndPoints{
   static const String baseUrl="https://fakestoreapi.com/";
   static const String login="auth/login";
   static const String register="users";
+  static const String getCart="carts/user/";
   static const String allProducts="products";
 
 }
