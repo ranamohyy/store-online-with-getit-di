2dd640732e2f1c25fffc7a3ed6984af491089c1d
ranamohyy <ranaamr22@gmail.com>
2025-09-09 03:31:23 +0300

    add function to hide and show password

--- lib/features/controllers/login_cubit/login_cubit.dart
+++ lib/features/controllers/login_cubit/login_cubit.dart
@@ -8,43 +8,27 @@ part 'login_state.dart';
 class LoginCubit extends Cubit<LoginState> {
   AuthRepo authRepo;
   LoginCubit(this.authRepo) : super(LoginInitial());
-  final TextEditingController nameController=TextEditingController();
-  final TextEditingController passwordController=TextEditingController();
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
 
+  bool obscureText = true;
 
-
-  bool obscureText= true;
-
-  void changeEyeIcon(){
-
+  void changeEyeIcon() {
     obscureText = !obscureText;
     emit(LoginInitial());
-
-
   }
 
-
-
-
-
-
-   void login()async{
-     emit(LoginLoading());
-   final Either<String,UserModel> response=
-   await authRepo.login(userName: nameController.text, password: passwordController.text);
-   response.fold(
-       (error) {
-         emit(LoginFailure(error));}
-     ,(r) {
-     emit(LoginSuccess());
-       },
-   );
-
+  void login() async {
+    emit(LoginLoading());
+    final Either<String, UserModel> response = await authRepo.login(
+        userName: nameController.text, password: passwordController.text);
+    response.fold(
+      (error) {
+        emit(LoginFailure(error));
+      },
+      (r) {
+        emit(LoginSuccess());
+      },
+    );
   }
-
-
-
-
-
-
 }
