9e7246727b88dffcbdc5b03e4f0a7fe32b531d4c
ranamohyy <ranaamr22@gmail.com>
2025-09-09 03:30:58 +0300

    add cart repo, cubit

--- lib/core/di/get_it.dart
+++ lib/core/di/get_it.dart
@@ -3,7 +3,9 @@ import"package:get_it/get_it.dart";
 import "package:implement_clean_code_for_store/core/helpers/cache_helper.dart";
 import "package:implement_clean_code_for_store/core/networking/dio_helper.dart";
 import "package:implement_clean_code_for_store/core/repo/auth_repo.dart";
+import "package:implement_clean_code_for_store/core/repo/cart_repo.dart";
 import "package:implement_clean_code_for_store/core/repo/home_repo.dart";
+import "package:implement_clean_code_for_store/features/controllers/cart_cubit/cart_cubit.dart";
 import "package:implement_clean_code_for_store/features/controllers/home_cubit/product_cubit.dart";
 import "package:implement_clean_code_for_store/features/controllers/login_cubit/login_cubit.dart";
   final getIt = GetIt.instance;
@@ -15,9 +17,11 @@ import "package:implement_clean_code_for_store/features/controllers/login_cubit/
     //repos
     getIt.registerLazySingleton<AuthRepo>(() => AuthRepo(getIt()),);
     getIt.registerLazySingleton<HomeRepo>(() => HomeRepo(getIt()),);
+    getIt.registerLazySingleton<CartRepo>(() => CartRepo(getIt()),);
 
     //cubit
     getIt.registerLazySingleton<LoginCubit>(() => LoginCubit(getIt()),);
     getIt.registerFactory<ProductCubit>(() => ProductCubit(getIt()));
+    getIt.registerFactory<CartCubit>(() => CartCubit(getIt()));
 
 }
