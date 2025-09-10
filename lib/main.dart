4b54260e08988acd1b58b3c7ca825677990d77c8
ranamohyy <ranaamr22@gmail.com>
2025-09-10 18:14:09 +0300

    start from splash

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,36 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
-import 'package:implement_clean_code_for_store/core/networking/dio_helper.dart';
-void main() {
+import 'package:implement_clean_code_for_store/features/views/splash/splash.dart';
+import 'core/di/get_it.dart';
+void main()async {
   WidgetsFlutterBinding.ensureInitialized();
-  DioHelper.initDio();
-  runApp(const MyApp());
+  setup();
+  await EasyLocalization.ensureInitialized();
+  runApp(
+    EasyLocalization(
+        supportedLocales: [Locale('en'), Locale('ar')],
+        path: 'assets/translations',
+        fallbackLocale: Locale('en'),
+        startLocale: Locale('en'),
+        child: MyApp()),
+  );
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      localizationsDelegates: context.localizationDelegates,
+      supportedLocales: context.supportedLocales,
+      locale: context.locale,
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home:Scaffold(),
+      home: SplashScreen(),
     );
   }
 }
